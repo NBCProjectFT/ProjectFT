9578aa13f4ebe371e79e1b246fbfc46dac2101e2
dltmdwo2323@gmail.com <sky_battle@naver.com>
2026-06-04 19:03:47 +0900

    프로젝트 초기 디렉토리 설정

---
+++ ProjectFT/Source/ProjectFT/Core/FTGameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FTGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTFT_API AFTGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
---
+++ ProjectFT/Source/ProjectFT/Interface/FTDamageable.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "FTDamageable.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UFTDamageable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class PROJECTFT_API IFTDamageable
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
