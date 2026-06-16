652df06b019165523a9325110b0bb58dc195cbc8
dltmdwo2323@gmail.com <sky_battle@naver.com>
2026-06-16 23:06:16 +0900

    GameplayMessageRouter 추가, 예시 코드 추가

---
+++ ProjectFT/Source/ProjectFT/Core/TestProcessor.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayMessageProcessor.h"
+#include "TestProcessor.generated.h"
+
+struct FTTestStruct;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PROJECTFT_API UTestProcessor : public UGameplayMessageProcessor
+{
+	GENERATED_BODY()
+
+public:
+	
+protected:
+	virtual void StartListening() override;
+	virtual void StopListening() override;
+	
+public:
+	void OnTestMessage(FGameplayTag Channel, const FTTestStruct& Payload);
+	
+};
