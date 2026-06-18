b155ee5e730d8b3a306defaf648a517200105b23
mooang lee <natalie12595@gmail.com>
2026-06-18 16:27:38 +0900

    feat: 보안 호출 이벤트 기반 추격 시스템 구현
    - Event.Security.Called 리스너 등록
    - SecurityCalled Payload 수신 처리
    - TargetActor 설정 로직 추가
    - StartChase 구현
    - MoveToActor 기반 플레이어 추격 기능 추가

--- ProjectFT/Source/ProjectFT/Security/FTSecurityAIController.h
+++ ProjectFT/Source/ProjectFT/Security/FTSecurityAIController.h
@@ -1,9 +1,16 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
-#include "Runtime/AIModule/Classes/AIController.h"
+#include "AIController.h"
+#include "Perception/AIPerceptionTypes.h"
+#include "GameplayTagContainer.h"
+#include "GameFramework/GameplayMessageSubsystem.h"
 #include "FTSecurityAIController.generated.h"
 
+class UAIPerceptionComponent;
+class UAISenseConfig_Sight;
+class AActor;
+struct FFTNPCReportPayloadStruct;
 UCLASS()
 class PROJECTFT_API AFTSecurityAIController : public AAIController
 {
@@ -14,7 +21,25 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
-
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "FT|Security")
+	TObjectPtr<UAIPerceptionComponent> SecurityPerceptionComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "FT|Security")
+	TObjectPtr<UAISenseConfig_Sight> SightConfig;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "FT|Security")
+	TObjectPtr<AActor> TargetActor;
+	
+	UFUNCTION()
+	void OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+	
 public:
-	virtual void Tick(float DeltaTime) override;
+	void SetTargetActor(AActor* NewTargetActor);
+	void StartChase();
+	
+private:
+	FGameplayMessageListenerHandle SecurityCalledListenerHandle;
+	void OnSecurityCalled(FGameplayTag Channel, const FFTNPCReportPayloadStruct& Payload);
 };
