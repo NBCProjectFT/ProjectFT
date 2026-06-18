1907f0f64cb4a46f1eb591c1d3bb4646626a18d9
dltmdwo2323@gmail.com <sky_battle@naver.com>
2026-06-18 17:32:02 +0900

    HUD UI 작업
    - 체력 바, 스태미너 바
    - 임시 퀵슬롯
    - 관련 에셋 Migration

--- ProjectFT/Source/ProjectFT/ViewModel/FTHUDViewModel.h
+++ ProjectFT/Source/ProjectFT/ViewModel/FTHUDViewModel.h
@@ -4,30 +4,101 @@
 #include "UObject/Object.h"
 #include "FTHUDViewModel.generated.h"
 
+class UTexture2D;
+
 UCLASS(BlueprintType)
-class PROJECTFT_API UFTHUDViewModel : public UObject
+class PROJECTFT_API UFTItemSlotDataObject : public UObject
 {
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(BlueprintReadWrite, Category = "FT|HUD")
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "FT|HUD|ItemSlot")
+	int32 SlotIndex = 0;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "FT|HUD|ItemSlot")
+	FName ItemId = NAME_None;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "FT|HUD|ItemSlot")
+	FText DisplayName;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "FT|HUD|ItemSlot")
+	int32 Quantity = 0;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "FT|HUD|ItemSlot")
+	TObjectPtr<UTexture2D> Icon = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "FT|HUD|ItemSlot")
+	bool bSelected = false;
+};
+
+UCLASS(BlueprintType)
+class PROJECTFT_API UFTHUDViewModel : public UObject
+{
+	GENERATED_BODY()
+public:
+	UFTHUDViewModel();
+	~UFTHUDViewModel();
+private:
+	//TODO Player로 부터 Stat 가져오기.
+	UPROPERTY(BlueprintReadWrite, Category = "FT|HUD", meta=(AllowPrivateAccess = "true"))
 	float HP = 100.0f;
 
-	UPROPERTY(BlueprintReadWrite, Category = "FT|HUD")
+	UPROPERTY(BlueprintReadWrite, Category = "FT|HUD", meta=(AllowPrivateAccess = "true"))
 	float Stamina = 100.0f;
 
-	UPROPERTY(BlueprintReadWrite, Category = "FT|HUD")
+	UPROPERTY(BlueprintReadWrite, Category = "FT|HUD", meta=(AllowPrivateAccess = "true"))
 	float CurrentWeight = 0.0f;
 
-	UPROPERTY(BlueprintReadWrite, Category = "FT|HUD")
+	UPROPERTY(BlueprintReadWrite, Category = "FT|HUD", meta=(AllowPrivateAccess = "true"))
 	float MaxWeight = 0.0f;
 
-	UPROPERTY(BlueprintReadWrite, Category = "FT|HUD")
+	UPROPERTY(BlueprintReadWrite, Category = "FT|HUD", meta=(AllowPrivateAccess = "true"))
 	float ReportGauge = 0.0f;
 
-	UPROPERTY(BlueprintReadWrite, Category = "FT|HUD")
+	UPROPERTY(BlueprintReadWrite, Category = "FT|HUD", meta=(AllowPrivateAccess = "true"))
 	FText ObjectiveText;
 
+	UPROPERTY(BlueprintReadOnly, Category = "FT|HUD", meta=(AllowPrivateAccess = "true"))
+	float TargetHPPercent = 1.0f;
+
+	UPROPERTY(BlueprintReadOnly, Category = "FT|HUD", meta=(AllowPrivateAccess = "true"))
+	float TargetStaminaPercent = 1.0f;
+
+	UPROPERTY(BlueprintReadOnly, Category = "FT|HUD|ItemSlot", meta=(AllowPrivateAccess = "true"))
+	TArray<TObjectPtr<UFTItemSlotDataObject>> ItemSlotObjects;
+
+public:
 	UFUNCTION(BlueprintCallable, Category = "FT|HUD")
 	void NotifyChanged();
+
+	UFUNCTION(BlueprintCallable, Category = "FT|HUD")
+	void SetHPPercent(float NewHPPercent);
+
+	UFUNCTION(BlueprintCallable, Category = "FT|HUD")
+	void SetHPValue(float CurrentHP, float MaxHP);
+
+	UFUNCTION(BlueprintCallable, Category = "FT|HUD")
+	void SetStamina(float CurrentStamina, float MaxStamina);
+
+	UFUNCTION(BlueprintCallable, Category = "FT|HUD")
+	void SetWeight(float NewCurrentWeight, float NewMaxWeight);
+
+	UFUNCTION(BlueprintCallable, Category = "FT|HUD")
+	void SetReportGauge(float NewReportGauge);
+
+	UFUNCTION(BlueprintCallable, Category = "FT|HUD")
+	void SetObjectiveText(const FText& NewObjectiveText);
+
+	UFUNCTION(BlueprintPure, Category = "FT|HUD")
+	float GetTargetHPPercent() const;
+
+	UFUNCTION(BlueprintPure, Category = "FT|HUD")
+	float GetTargetStaminaPercent() const;
+
+public:
+	void TestCode();
+	
+private:
+	UFTItemSlotDataObject* GetOrCreateItemSlot(int32 SlotIndex);
+	float NormalizePercent(float Value) const;
 };
