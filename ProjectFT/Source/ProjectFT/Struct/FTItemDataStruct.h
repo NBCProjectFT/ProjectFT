5e95cc93647d19c964a81db3d637a0e29362dacc
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2026-06-18 17:17:33 +0900

    feat: 아이템 기본 틀 구현 완료
    - 아이템 카테고리 추가
    - 아이템 데이터 구조체 추가
    - 아이템 인터페이스 추가
    - 아이템 기본 액터 클래스 추가
    - 아이템 데이터 에셋 추가

---
+++ ProjectFT/Source/ProjectFT/Struct/FTItemDataStruct.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectFT/Enum/FTItemCategoryType.h"
+#include "FTItemDataStruct.generated.h"
+
+USTRUCT(BlueprintType)
+struct PROJECTFT_API FTItemDataStruct
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Item")
+	FName ItemId = NAME_None;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Item")
+	FText ItemName;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Item")
+	FText ItemDescription;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Item")
+	EFTItemCategoryType ItemCategory = EFTItemCategoryType::Basic;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Item")
+	float Weight = 0.0f;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Item")
+	int32 MaxStack = 1;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Item")
+	TSoftObjectPtr<UTexture2D> ItemIcon;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Item")
+	TSoftObjectPtr<UStaticMesh> ItemMesh;
+};
