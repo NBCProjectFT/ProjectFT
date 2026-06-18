17c351885d21d9f255485c6de329ed6e2229459a
문승종 <jongcc2345@gmail.com>
2026-06-18 17:39:26 +0900

    Squashed commit of the following:
    commit 91fdad16aec13bc015d7660a22337c55b5085b59
    Merge: 87b40b8 7c015a7
    Author: goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
    Date:   Thu Jun 18 17:19:05 2026 +0900
    
        Merge pull request #4 from leeseungjae97/feat/janggoeun
    
        feat: 아이템 기본 틀 구현 완료
    
    commit 7c015a773e1b34130fa9b57f609feb5f48d218a1
    Author: goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
    Date:   Thu Jun 18 17:17:33 2026 +0900
    
        feat: 아이템 기본 틀 구현 완료
    
        - 아이템 카테고리 추가
        - 아이템 데이터 구조체 추가
        - 아이템 인터페이스 추가
        - 아이템 기본 액터 클래스 추가
        - 아이템 데이터 에셋 추가

---
+++ ProjectFT/Source/ProjectFT/Interface/FTInteractable.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "Coreminimal.h"
+#include "UObject/Interface.h"
+#include "FTInteractable.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UFTInteractable : public UInterface
+{
+	GENERATED_BODY()	
+};
+
+class PROJECTFT_API IFTInteractable
+{
+	GENERATED_BODY()
+	
+public:
+	/*
+	 * @brief : 플레이어가 아이템을 줍거나, 매대와 상호작용할 때 사용할 메서드입니다.
+	 * @Param Instigator : 상호작용을 시도하는 액더(ex: 플레이어)
+	 * @return : 상호작용 성공 여부
+	 */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category="Interaction")
+	bool Interact(AActor* InInstigator);
+};
