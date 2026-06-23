0e812f7d1cae52ed38a770849b1abdfa2f032861
mooang lee <natalie12595@gmail.com>
2026-06-23 20:51:47 +0900

    손님NPC AIController, State Tree 추가
    - 플레이어 감지 용 손님 NPC 시야각
    - 플레이어 추적
    - 쇼핑, 의심, 신고 상태 구현
    - GamplayMessageSubsystem 방송 메시지
    - AIController 적용
    - State Tree 구현 및 STT 추가
    - 임시 테스트 용 AFTShoppingPoint 액터

---
+++ ProjectFT/Source/ProjectFT/NPC/FTShoppingPoint.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FTShoppingPoint.generated.h"
+
+UCLASS()
+class PROJECTFT_API AFTShoppingPoint : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AFTShoppingPoint();
+};
