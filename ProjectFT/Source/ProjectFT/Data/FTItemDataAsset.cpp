0f498f572b88fbde328c9621eb5397757de06d1f
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2026-06-24 14:00:45 +0900

    인벤토리 컴포넌트에 필요한 함수 추가 및 주석 통일화
    - MaxStack을 초과할 경우 슬롯을 분리시켰었는데, 이 방식에서 분리하지 않는 방식으로 수정
    - 주석 수정
    - 특정 아이템의 UFTItemDataAsset 메모리 주소를 반환하는 함수 추가
    - 특정 아이템의 현재 누적된 총 수량을 반환하는 함수 추가

--- ProjectFT/Source/ProjectFT/Data/FTItemDataAsset.cpp
+++ ProjectFT/Source/ProjectFT/Data/FTItemDataAsset.cpp
@@ -2,7 +2,7 @@
 
 FPrimaryAssetId UFTItemDataAsset::GetPrimaryAssetId() const
 {
-	// ItemDatabase의 Key로 설정한 ItemId 혹은 에셋명을 고유 식별 명칭(Asset Name)으로 지정합니다.
+	// 에셋 매니저(Asset Manager) 시스템에서 이 데이터 에셋을 식별하고 동적으로 로드하기 위한 고유한 등록증(식별표)을 발급해주는 함수
 	// FName("FTItemItem")은 DefaultGame.ini에 지정한 AssetType과 일치해야 합니다.
 	return FPrimaryAssetId(FName("FTItemItem"), ItemData.ItemId);
 }
