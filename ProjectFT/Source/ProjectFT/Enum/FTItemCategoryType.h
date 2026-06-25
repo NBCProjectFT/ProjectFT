ad04db75755d242baa07c5b1be4ea045cb827150
goeun441 <120786130+Jang-Goeun@users.noreply.github.com>
2026-06-25 13:44:43 +0900

    FTItemCategoryType 생성 후 FTItemDataStruct에 가격, 카테고리 속성 추가

---
+++ ProjectFT/Source/ProjectFT/Enum/FTItemCategoryType.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "FTItemCategoryType.generated.h"
+
+UENUM(BlueprintType)
+enum class EFTItemCategoryType : uint8
+{
+	None            UMETA(DisplayName = "None"),
+	Common            UMETA(DisplayName = "Common"),
+	Weapon            UMETA(DisplayName = "Weapon"),
+	Healing            UMETA(DisplayName = "Healing")
+};
