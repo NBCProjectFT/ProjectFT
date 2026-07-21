c165a42b6a8fde9ca1bfaee0f6b70f2b795c5468
wns6677 <wns96096@gmail.com>
2026-07-21 15:34:17 +0900

    아이템 타일 bp화

--- ProjectFT/Source/ProjectFT/UI/HubUI/FTItemTileEntryWidget.h
+++ ProjectFT/Source/ProjectFT/UI/HubUI/FTItemTileEntryWidget.h
@@ -5,36 +5,8 @@
 #include "Blueprint/UserWidget.h"
 #include "FTItemTileEntryWidget.generated.h"
 
-class UImage;
-class UTextBlock;
-class UCheckBox;
-class UFTItemTileListObject;
-
 UCLASS()
 class PROJECTFT_API UFTItemTileEntryWidget : public UUserWidget, public IUserObjectListEntry
 {
 	GENERATED_BODY()
-
-protected:
-	virtual void NativeOnListItemObjectSet(UObject* ListItemObject) override;
-	virtual void NativeOnItemSelectionChanged(bool bIsSelected) override;
-
-	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
-	UImage* IMG_ItemIcon;
-
-	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
-	UTextBlock* TXT_ItemName;
-
-	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
-	UTextBlock* TXT_ItemCount;
-
-	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
-	UTextBlock* TXT_ItemWeight;
-
-	UPROPERTY(BlueprintReadOnly, meta = (BindWidgetOptional))
-	UCheckBox* CHK_ItemSelected;
-
-private:
-	UPROPERTY(Transient)
-	TObjectPtr<UFTItemTileListObject> CurrentTileObject;
 };
